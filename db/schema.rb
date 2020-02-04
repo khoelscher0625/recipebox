ee157d9a4bfccfd0e1abfb6bf8c060c73bf92c6d
Kathryn Hoelscher <khoelscher0625@gmail.com>
2020-02-04 03:18:37 +0000

    changed table column name

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_04_025935) do
+ActiveRecord::Schema.define(version: 2020_02_04_031246) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
-    t.string "type"
+    t.string "course"
     t.integer "servings"
     t.text "memory"
     t.datetime "created_at", null: false
